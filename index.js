c4dba502eeabf2cc24ab9b938da7765c7d7c98a3
Antony O'Neill <antony@ant-web.co.uk>
2021-04-16 10:36:40 +0100

    Add transform

---
+++ index.js
@@ -0,0 +1,23 @@
+export default function (babel) {
+  const { types: t } = babel;
+
+  return {
+    visitor: {
+      MemberExpression: {
+        exit({ node }) {
+          const prop = node.property;
+          if (
+            !node.computed &&
+            prop.name === 'default'
+          ) {
+            console.log(prop)
+            // foo.default -> foo["default"]
+            node.property = t.stringLiteral(prop.name)
+            node.computed = true;
+          }
+        },
+      },
+    }
+  };
+}
+
